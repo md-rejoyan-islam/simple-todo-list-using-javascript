40718ca106d34b7f198ca8006c7ff250d30873bf
RejoyanIslam <rejoyanislam0014@gmail.com>
2023-11-08 20:39:51 +0600

    task save in local strorage

--- js/app.js
+++ js/app.js
@@ -5,7 +5,38 @@ const addTask = document.getElementById("add-task");
 const clearAllTask = document.getElementById("clear-all-task");
 
 // filter task input
-const filterTask = document.getElementById("filter-task");
+const filterTask = document.getElementById("filter-task") || [];
+
+// local storage task
+const tasks = JSON.parse(localStorage.getItem("tasks"));
+
+// check if tasks is not empty
+if (tasks) {
+  tasks.forEach((task) => {
+    const newTask = document.createElement("li");
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "X";
+    deleteBtn.classList.add(
+      "delete-btn",
+      "bg-red-500",
+      "text-white",
+      "py-1",
+      "px-2",
+      "rounded-md",
+      "ml-2"
+    );
+
+    deleteBtn.addEventListener("click", deleteTask);
+
+    newTask.textContent = task;
+    newTask.appendChild(deleteBtn);
+
+    newTask.classList.add("task", "list-inside", "list-decimal");
+    const list = document.querySelector(".task-list");
+    clearAllTask.classList.remove("hidden");
+    list.appendChild(newTask);
+  });
+}
 
 addTask.addEventListener("submit", handleSubmit);
 clearAllTask.addEventListener("click", clearTasks);
@@ -21,6 +52,11 @@ function handleSubmit(e) {
 
   // check if task is empty
   if (!task) return alert("Please enter a task");
+
+  // save task to local storage
+  tasks.push(task);
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 
   const newTask = document.createElement("li");
   const deleteBtn = document.createElement("button");
@@ -60,6 +96,12 @@ function deleteTask(e) {
   e.preventDefault();
   const list = document.querySelector(".task-list");
   list.removeChild(e.target.parentElement);
+
+  // remove task from local storage
+  const task = e.target.parentElement.textContent;
+  const index = tasks.indexOf(task);
+  tasks.splice(index, 1);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
 // filter task by keyword
